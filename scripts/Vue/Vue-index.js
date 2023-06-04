b4d2b65ff50a9b60233a361cfe3630f0729ebc5b
serchonodera117 <sergio.lobo117@gmail.com>
2023-06-03 23:07:45 -0600

    fixed the bug of buttom menu, and added the functionality of change ifrme view

--- scripts/Vue/Vue-index.js
+++ scripts/Vue/Vue-index.js
@@ -2,7 +2,25 @@ let App = {
    data(){
         return {
             isTabLeftMenuVisible: true,
+            routes:{
+                cards: 'pages/cards.html',
+                menu: 'pages/menus.html',
+                buttoms: '',
+                images: '',
+            }
         }
+   },
+   methods: {
+     changeIframe(id){
+        let iframe = document.getElementById('iframe-pages');
+        switch(id) {
+            case 'o1': iframe.src =this.routes.cards; break;
+            case 'o2': iframe.src = this.routes.menu; break;
+            case 'o3': iframe.src = this.routes.buttoms; break;
+            case 'o4': iframe.src = this.routes.images; break;
+            default: iframe.src = ''; break;
+        }
+     }
    }
 };
 
